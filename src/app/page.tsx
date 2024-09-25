00acbbb7fd78922d9abc83c860f2af7c545c0538
Yogesh Bisht <reinzire@gmail.com>
2024-09-25 21:59:52 +0530

    call to action component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/app/sections/Hero";
 import LogoTicker from "@/components/LogoTicker";
 import Features from "./sections/Features";
 import Testimonials from "./sections/Testimonials";
+import CallToAction from "./sections/CallToAction";
 export default function Home() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function Home() {
       <LogoTicker />
       <Features />
       <Testimonials />
+      <CallToAction />
     </>
   );
 }
